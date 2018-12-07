d5ff1bdf99ec9dbeaa6eb983eba3a7ce6c2e23fa
zhaoqunxing <295401986@qq.com>
2018-12-07 10:33:34 +0800

    Create install.AMQP-CPP-4.0.1.sh

---
+++ install.AMQP-CPP-4.0.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#AMQP-CPP-4.0.1
+
+export CXXFLAGS=-m32  
+export CFLAGS=-m32  
+
+rm -rf build
+mkdir build \
+  &&cd build \
+  && cmake .. -DAMQP-CPP_BUILD_SHARED=ON -DAMQP-CPP_LINUX_TCP=ON -DAMQP-CPP_BUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug \
+  && cmake --build . \
