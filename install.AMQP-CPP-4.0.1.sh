79e5fc771af44ff5a108594769a02a623a159a57
zhaoqunxing <295401986@qq.com>
2018-12-10 16:03:31 +0800

    Update install.AMQP-CPP-4.0.1.sh

--- install.AMQP-CPP-4.0.1.sh
+++ install.AMQP-CPP-4.0.1.sh
@@ -9,4 +9,4 @@ rm -rf build
 mkdir build \
   &&cd build \
   && cmake .. -DAMQP-CPP_BUILD_SHARED=ON -DAMQP-CPP_LINUX_TCP=ON -DAMQP-CPP_BUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug \
-  && cmake --build . \
+  && cmake --build . --target install
